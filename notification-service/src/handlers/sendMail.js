03721f1347ff510b80f737098f007974800bc563
Ravinder Chahal <chahaltechie@gmail.com>
2021-05-19 17:15:59 +0530

    test

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({region: 'us-east-1'});
 
 async function sendMail(event, context) {
+    console.log(event);
+    const record = event.Records[0];
+    console.log(record);
+    
+    const recordBody = JSON.parse(record.body);
+    const { subject, body, recipient } = recordBody;
     const params = {
         Source: 'chahaltechie@gmail.com',
         Destination: {
-            ToAddresses: ['chahal.420@gmail.com'],
+            ToAddresses: [recipient],
         },
         Message: {
             Body: {
                 Text: {
-                    Data: 'Hello from aws!',
+                    Data: body,
                 },
             },
             Subject: {
-                Data: 'Test Mail',
+                Data: subject,
             },
         },
     };
